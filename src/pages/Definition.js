b0e1e426374d3aa13e31a4458555fbe52d1b9cad
Nisar Ahmed <drnisar@hotmail.com>
2023-06-27 23:12:33 +0500

    dictionary working

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,30 +1,40 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { v4 as uuid } from "uuid";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 import NotFound from "./NotFound";
 
 export default function Definition() {
   const [word, setWord] = useState([]);
   const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
   let { search } = useParams();
   const navigate = useNavigate();
   useEffect(() => {
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search)
+    // const url = "fdhdfjldk.com";
+    // const url = "https://httpstat.us/401";
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
+
+    fetch(url)
       .then((response) => {
-        if (response.status === 404) {
-          setNotFound(true);
+        if (!response.ok) {
+          setError(true);
+          throw new Error("Somethingggggg went Wrong... ");
         }
         return response.json();
       })
       .then((data) => {
         setWord(data[0].meanings);
+      })
+      .catch((e) => {
+        return "";
       });
   }, []);
 
-  if (notFound === true) {
+  if (error === true) {
     return (
       <>
-        <NotFound />
+        <p>Something went wrong ...</p>
+        <Link to="/dictionary">search again</Link>
       </>
     );
   }
@@ -33,7 +43,7 @@ export default function Definition() {
       {word
         ? word.map((meaning) => {
             return (
-              <p key={uuid()}>
+              <p key={uuidv4()}>
                 {meaning.partOfSpeech + ": "}
                 {meaning.definitions[0].definition}
               </p>
